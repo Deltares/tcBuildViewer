b53785c1731417a2aa25b73d05f247d70874d4c2
Robin van Westrenen <Robin.vanWestrenen@deltares.nl>
2022-12-09 16:00:02 +0100

    User login handling.

---
+++ user.js
@@ -0,0 +1,22 @@
+function getCurrentUser() {
+    fetch(`${teamcity_base_url}/app/rest/users/current`, {
+        headers: {
+            'Accept': 'application/json',
+        },
+        credentials: 'include',
+    })
+        .then((result) => {
+            if (result.ok) {
+                return result.json();
+            } else {
+                console.log("User is not logged in to TeamCity.");
+                showLogin();
+            }
+            
+        })
+        .catch(err => { return null; })
+}
+
+function showLogin() {
+    document.getElementById('login').classList.toggle('hidden');
+}
