f389e00fc810c7f0e2ba754437d0222686e0e9c6
Robin van Westrenen <Robin.vanWestrenen@deltares.nl>
2022-12-09 17:34:24 +0100

    Wait for user login loop.

--- user.js
+++ user.js
@@ -1,6 +1,24 @@
 async function getCurrentUser() {
 
+    if (!userLoggedIn()) {
+        showLogin();
+        do {
+            await new Promise(r => setTimeout(r, 2000));
+        } while (!userLoggedIn());
+    }
+
+    return user;
+
+}
+
+function showLogin() {
+    document.getElementById('login').classList.toggle('hidden');
+}
+
+async function userLoggedIn() {
+
     try {
+
         var response = await fetch(`${teamcity_base_url}/app/rest/users/current`, {
             headers: {
                 'Accept': 'application/json',
@@ -10,16 +28,14 @@ async function getCurrentUser() {
 
         if (response && response.ok) {
             user = response.json();
-            return user;
+            return true;
         } else {
-            return null;
+            return false;
         }
+        
     } catch (err) {
         console.log(err);
-        return null;
+        return false;
     }
-}
 
-function showLogin() {
-    document.getElementById('login').classList.toggle('hidden');
 }
