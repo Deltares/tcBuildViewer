9d968046f458937a2e5e7a412b31bb0791a796a8
Robin van Westrenen <Robin.vanWestrenen@deltares.nl>
2022-12-09 17:22:50 +0100

    Catch CORS error.

--- user.js
+++ user.js
@@ -1,21 +1,27 @@
 async function getCurrentUser() {
 
-    var response = await fetch(`${teamcity_base_url}/app/rest/users/current`, {
-        headers: {
-            'Accept': 'application/json',
-        },
-        credentials: 'include',
-    })
-    .catch(err => {
-        user = null;
-        showLogin();
-    });
+    try {
+        var response = await fetch(`${teamcity_base_url}/app/rest/users/current`, {
+            headers: {
+                'Accept': 'application/json',
+            },
+            credentials: 'include',
+        })
+        .catch(err => {
+            user = null;
+            showLogin();
+        });
 
-    if (response && response.ok) {
-        user = response.json();
-        return user;
-    } else {
+        if (response && response.ok) {
+            user = response.json();
+            return user;
+        } else {
+            showLogin();
+            return null;
+        }
+    } catch (err) {
         showLogin();
+        console.log(err);
         return null;
     }
 }
