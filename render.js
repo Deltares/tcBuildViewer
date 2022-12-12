091e36d0a66da6581c5f1e83a9e2dedb48d6519d
Robin van Westrenen <Robin.vanWestrenen@deltares.nl>
2022-12-12 18:40:24 +0100

    typo

--- render.js
+++ render.js
@@ -134,7 +134,7 @@ function renderBuild(build) {
     // Link to TeamCity build page.
     var buildLink = document.createElement("a");
     //buildLink.setAttribute('href', build.webUrl);
-    buildLink.setAttribute('onclick', get_messages_for_build(buidl.id));
+    buildLink.setAttribute('onclick', get_messages_for_build(build.id));
     buildLink.setAttribute('target', '_blank');
     buildLink.setAttribute('title', `Status: ${build.status}\nID ${build.id}\n# ${build.number}\nFinished ${new Date(build.unixTime).toLocaleString()}\n${build.statusText}`);
     buildDiv.appendChild(buildLink);
