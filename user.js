a62e65b518ed06422f3eb3e6848c344083aa6267
robin-deltares <118373690+robin-deltares@users.noreply.github.com>
2022-12-12 10:29:46 +0100

    11 higher level domain name cookies are sent to tcbv (#12)
    * Put cookie in var.
    
    * bug

--- user.js
+++ user.js
@@ -94,3 +94,25 @@ function updateFormSettings() {
     settings_textarea.value = JSON.stringify(edit_settings, undefined, 2);
     settingsDiv.innerText = JSON.stringify(settings, undefined, 2);
 }
+
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+  
+  function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
