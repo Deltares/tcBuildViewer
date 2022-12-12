07be3371763f9d87e14600f6d2aa9b182003a451
Robin van Westrenen <Robin.vanWestrenen@deltares.nl>
2022-12-12 13:18:19 +0100

    debug

--- projects.js
+++ projects.js
@@ -63,12 +63,12 @@ function add_builds_to_buildtype(buildType) {
             buildType.builds = output;
             renderBuildType(buildType);
             if (buildType.builds.build) {
-                // Check if the build result is changed with the last build.
+                /* Check if the build result is changed with the last build.
                 if (buildType.builds.build.length > 1 && buildType.builds.build[0].status != buildType.builds.build[1].status) {
                     buildType.builds.build.statusChanged = true;
                 } else {
                     buildType.builds.build.statusChanged = false;
-                }
+                }*/
                 Object.entries(buildType.builds.build).forEach(([key, build]) => {
 
                     if (build.finishOnAgentDate)
