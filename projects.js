bcd837fa52a814206a674bba5abce2c09db21a25
Robin van Westrenen <Robin.vanWestrenen@deltares.nl>
2022-12-12 22:16:44 +0100

    fix

--- projects.js
+++ projects.js
@@ -77,7 +77,7 @@ function add_builds_to_buildtype(buildType) {
                     if (build.finishOnAgentDate)
                         build.unixTime = tcTimeToUnix(build.finishOnAgentDate);
 
-                    if (build.unixTime > cutoffUnixTime())
+                    //if (build.unixTime > cutoffUnixTime())
                         renderBuild(build);
 
                 });
