1844afcba157ecfbb4ea7ce282a4cb3b6ccf90e5
Robin van Westrenen <Robin.vanWestrenen@deltares.nl>
2022-12-12 16:36:17 +0100

    debug

--- render.js
+++ render.js
@@ -147,7 +147,9 @@ function renderBuild(build) {
 
 function renderMessages(buildId,messages) {
     var parentElementId = document.getElementById(buildId).parentElement.id;
-    var buildStepsText = document.querySelectorAll(`#${parentElementId} > .buildSteps`)[0];
+    var buildStepsText = document.querySelectorAll(`#${parentElementId} > .buildSteps`);
+    console.log(`#${parentElementId} > .buildSteps`);
+    console.log(buildStepsText);
     buildStepsText.innerHTML = JSON.stringify(buildStepsText);
 }
 
