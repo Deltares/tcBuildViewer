56ed7ae48a525f9fc6756dc2a4770809e9d1be1c
Robin van Westrenen <Robin.vanWestrenen@deltares.nl>
2022-12-13 13:04:34 +0100

    fix

--- projects.js
+++ projects.js
@@ -65,15 +65,14 @@ function add_builds_to_buildtype(buildType) {
             buildType.builds = output;
             // Check if the build result is changed with the last build.
             
-            if (buildType.builds.build && buildType.builds.build.problemOccurrences && buildType.builds.build[0].problemOccurrences.newFailed > 0) {
+            if (buildType.builds.build && buildType.builds.build[0].problemOccurrences && buildType.builds.build[0].problemOccurrences.newFailed && buildType.builds.build[0].problemOccurrences.newFailed > 0) {
                 buildType.statusChanged = true;
                 
             } else if (buildType.builds.build && buildType.builds.build.length > 1 && buildType.builds.build[0].status != buildType.builds.build[1].status) {
                 buildType.statusChanged = true;
-            } else if (buildType.builds.build && buildType.builds.build.length > 1 && buildType.builds.build.testOccurrences && buildType.builds.build[0].testOccurrences.passed != buildType.builds.build[1].testOccurrences.passed) {
+            } else if (buildType.builds.build && buildType.builds.build.length > 1 && buildType.builds.build[0].testOccurrences && buildType.builds.build[0].testOccurrences.passed != buildType.builds.build[1].testOccurrences.passed) {
                 buildType.statusChanged = true;
             } else {
-                console.log((buildType.builds.build[0].problemOccurrences.newFailed > 0));
                 buildType.statusChanged = false;
             }
             renderBuildType(buildType);
