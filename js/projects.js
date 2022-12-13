0f975a52697b8bcda37bcd5bd38cdfc114440e5d
Robin van Westrenen <Robin.vanWestrenen@deltares.nl>
2022-12-13 22:33:01 +0100

    fix

--- js/projects.js
+++ js/projects.js
@@ -85,24 +85,24 @@ function add_builds_to_buildtype(buildType) {
             }
             renderBuildType(buildType);
             if (buildType.builds.build) {
+                
+                for (i=0; i<buildType.builds.build.length; i++) {
 
-                Object.entries(buildType.builds.build).forEach(([key, build]) => {
-
-                    if (buildType.builds.build[key+1]) {
-                        console.log("bla");
-                        if (buildType.builds.build[key+1].testOccurrences && build.testOccurrences &&
-                            buildType.builds.build[key+1].testOccurrences.passed != build.testOccurrences.passed) {
-                            build.statusChanged = true;
+                    if (buildType.builds.build[i+1]) {
+                        
+                        if (buildType.builds.build[i].testOccurrences && buildType.builds.build[i+1].testOccurrences &&
+                            buildType.builds.build[i].testOccurrences.passed != buildType.builds.build[i+1].testOccurrences.passed) {
+                            buildType.builds.build[i].statusChanged = true;
                         }
                     }
 
-                    if (build.finishOnAgentDate) {
-                        build.unixTime = tcTimeToUnix(build.finishOnAgentDate);
+                    if (buildType.builds.build[i].finishOnAgentDate) {
+                        buildType.builds.build[i].unixTime = tcTimeToUnix(buildType.builds.build[i].finishOnAgentDate);
                     }
 
-                    renderBuild(build);
+                    renderBuild(buildType.builds.build[i]);
 
-                });
+                };
 
             }
         })
