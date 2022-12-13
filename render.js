c1b8840601c3a9b1a5b656f4fb1dcf10d8018eb7
Robin van Westrenen <Robin.vanWestrenen@deltares.nl>
2022-12-13 09:28:41 +0100

    typo

--- render.js
+++ render.js
@@ -55,7 +55,7 @@ function renderProject(project) {
 
     // Collapse button text.
     var collapseDivText = document.createTextNode('▼');
-    projecollapseDiv.appendChild(collapseDivText);
+    collapseDiv.appendChild(collapseDivText);
 
 }
 
