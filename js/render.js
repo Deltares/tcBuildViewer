637a7fc970cea43ebd54983b6121f93a8d4567b4
Robin van Westrenen <Robin.vanWestrenen@deltares.nl>
2022-12-14 20:46:21 +0100

    bla

--- js/render.js
+++ js/render.js
@@ -251,7 +251,7 @@ function renderBuildDetails(buildId,messages,changes) {
         var fileList = change.files.file.map(file => file.relative-file);
         console.log(fileList);
         linkDiv.innerHTML = `<a href='${change.webUrl}' title='${fileList}'>#${change.comment}</a>`;
-        userDiv.innerHTML = `<span class='build_user_name'>${change.user?change.user.name:'🤷🏼‍♀️'}</span>`;
+        userDiv.innerHTML = `<span class='build_user_name'>${change.user?change.user.name:'🤖'}</span>`;
         timeDiv.innerHTML = `<span class='build_time smaller'>(${new Date(tcTimeToUnix(change.date)).toLocaleString()})</span>`;
         changesDiv.appendChild(versionDiv);
         changesDiv.appendChild(linkDiv);
