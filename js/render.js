4c3bb145da7776180aa1c51d38abc9394c3fe196
Robin van Westrenen <Robin.vanWestrenen@deltares.nl>
2022-12-14 20:10:02 +0100

    smaller

--- js/render.js
+++ js/render.js
@@ -244,7 +244,7 @@ function renderBuildDetails(buildId,messages,changes) {
         versionDiv.innerHTML = `#${change.version}`;
         var fileList = change.files.file.map(file => file.relative-file);
         linkDiv.innerHTML = `<a href='${change.webUrl}' title='${fileList}'>#${change.comment}</a>`;
-        userDiv.innerHTML = `${change.user.name} (${new Date(tcTimeToUnix(change.date)).toLocaleString()})`;
+        userDiv.innerHTML = `${change.user.name}<br /><span class='smaller'>(${new Date(tcTimeToUnix(change.date)).toLocaleString()})</span>`;
         changesDiv.appendChild(versionDiv);
         changesDiv.appendChild(linkDiv);
         changesDiv.appendChild(userDiv);
