f4dab23cd6e43db7090305fcb6b6c058cc9450c2
Robin van Westrenen <Robin.vanWestrenen@deltares.nl>
2022-12-18 20:41:00 +0100

    t

--- js/render.js
+++ js/render.js
@@ -146,7 +146,6 @@ function renderBuildType(buildType) {
     // Test statistics
     if (buildType.builds.build[0].testOccurrences) {
         let testOccurrences = buildType.builds.build[0].testOccurrences
-        //console.log(testOccurrences)
         let newFailed = testOccurrences.newFailed?testOccurrences.newFailed:0
         let muted = testOccurrences.muted?testOccurrences.muted:0
         let passed = testOccurrences.passed?testOccurrences.passed:0
