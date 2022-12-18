4b218b77d7dfea91a4a166a26e6166e0626d9954
Robin van Westrenen <Robin.vanWestrenen@deltares.nl>
2022-12-18 20:22:36 +0100

    t

--- js/projects.js
+++ js/projects.js
@@ -112,7 +112,7 @@ async function add_builds_to_buildtype(buildType, project) {
                     project.testNewFailed += build[0].testOccurrences.newFailed?build[0].testOccurrences.newFailed:0
                     project.testMuted     += build[0].testOccurrences.muted?build[0].testOccurrences.muted:0
                     project.testPassed    += build[0].testOccurrences.passed?build[0].testOccurrences.passed:0
-                    project.testCount     += build[0].testOccurrences.count?build[0].testOccurrences.count:0
+                    project.testCount     += (build[0].testOccurrences.count?build[0].testOccurrences.count:0)-project.testMuted
                 }
 
                 for (i=0; i<build.length; i++) {
