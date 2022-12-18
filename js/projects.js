dd9181e3e485d6ca4d84cefc9718b70b439e242a
Robin van Westrenen <Robin.vanWestrenen@deltares.nl>
2022-12-18 14:14:07 +0100

    shorter

--- js/projects.js
+++ js/projects.js
@@ -76,7 +76,7 @@ function add_builds_to_buildtype(buildType) {
             buildType.builds = output;
             // Check if the build result is changed with the last build.
             
-            if (buildType.builds.build?.length > 0 && buildType.builds.build[0].problemOccurrences?.newFailed > 0) {
+            if (buildType.builds.build?.[0]?.problemOccurrences?.newFailed > 0) {
                 buildType.statusChanged = true;
                 
             } else if (buildType.builds.build?.length > 1 && buildType.builds.build[0].status != buildType.builds.build[1].status) {
