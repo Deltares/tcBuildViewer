21fb56026252c1c2e25bd65428389dd8afd63aca
Robin van Westrenen <Robin.vanWestrenen@deltares.nl>
2022-12-18 20:11:18 +0100

    t

--- js/render.js
+++ js/render.js
@@ -88,7 +88,7 @@ function renderProject(project) {
 
 function renderProjectTestStatistics(project) {
     if(project.testCount) {
-        project.testPercentage = Sumber((project.testPassed/project.testCount)*100).toFixed(2)
+        project.testPercentage = Number((project.testPassed/project.testCount)*100).toFixed(2)
         let testStatisticsSumText = document.createTextNode(`${project.testNewFailed?'('+project.testNewFailed+' new) ':''}${project.testMuted?'('+project.testMuted+' muted) ':''}[${project.testPassed?project.testPassed:0}/${project.testCount}] = ${project.testPercentage}%`)
         project.div.getElementsByClassName('project_title')[0].append(testStatisticsSumText)
     }
