c6a78082aec2e34460b49b4b607622b82b0c8d06
Robin van Westrenen <Robin.vanWestrenen@deltares.nl>
2022-12-18 20:02:50 +0100

    t

--- js/projects.js
+++ js/projects.js
@@ -87,7 +87,7 @@ async function add_builds_to_buildtype(buildType, project) {
     // Will enable/disable buttons when there are downloads in progress.
     checkFilterButtons(++download_queue_length)
 
-    return fetch(`${teamcity_base_url}/app/rest/builds?locator=defaultFilter:false,state:(finished:true),buildType:(id:${buildType.id}),startDate:(date:${cutoffTcString()},condition:after),count:${build_count}&${buildType_fields}`, {
+    return await fetch(`${teamcity_base_url}/app/rest/builds?locator=defaultFilter:false,state:(finished:true),buildType:(id:${buildType.id}),startDate:(date:${cutoffTcString()},condition:after),count:${build_count}&${buildType_fields}`, {
         headers: {
             'Accept': 'application/json',
         },
