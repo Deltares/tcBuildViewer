cb87662aacf696e87d71f9ea32d19023aba77fc8
Robin van Westrenen <Robin.vanWestrenen@deltares.nl>
2022-12-22 19:49:08 +0100

    t

--- js/render.js
+++ js/render.js
@@ -37,7 +37,7 @@ async function renderProject(project) {
     let projectDiv = document.createElement("div")
     let parentElement = document.getElementById(project.parentProjectId)
     if (parentElement) {
-        projectDiv.style.order = project.order*3
+        projectDiv.style.gridRow = project.order*2
         parentElement.appendChild(projectDiv)
     } else {
         document.getElementById(`${project.id}_wrapper`).appendChild(projectDiv)
@@ -113,9 +113,9 @@ async function renderBuildType(buildType) {
         return
 
     // Add buildType to project.
-    let buildTypeDiv = document.createElement("div")
+    //let buildTypeDiv = document.createElement("div")
     let parentElement = document.getElementById(buildType.projectId).getElementsByClassName('projectBuildTypesDiv')[0]
-    buildTypeDiv.style.order = buildType.order
+    //buildTypeDiv.style.order = buildType.order
     //parentElement.appendChild(buildTypeDiv)
 
     let buildTypeLink = document.createElement("a")
@@ -126,7 +126,7 @@ async function renderBuildType(buildType) {
     buildTypeLink.classList.add('buildType')
     buildTypeLink.classList.add('buildTypePart')
     buildTypeLink.classList.add(buildType.projectId)
-    buildTypeLink.style.order = (buildType.order*3)
+    buildTypeLink.style.gridRow = buildType.order*2
 
     parentElement.appendChild(buildTypeLink)
     // Add status of last build as class.
@@ -152,7 +152,7 @@ async function renderBuildType(buildType) {
     let testStatisticsDiv = document.createElement('div')
     testStatisticsDiv.classList.add('test_statistics_text')
     testStatisticsDiv.classList.add('buildTypePart')
-    testStatisticsDiv.style.order = (buildType.order*3)+1
+    testStatisticsDiv.style.gridRow = buildType.order*2
     parentElement.appendChild(testStatisticsDiv)
 /*
     // Test statistics
@@ -185,7 +185,7 @@ async function renderBuildType(buildType) {
     buildListDiv.setAttribute('id', buildType.id + '_buildList')
     buildListDiv.classList.add('buildList')
     buildListDiv.classList.add('buildTypePart')
-    testStatisticsDiv.style.order = (buildType.order*3)+2
+    testStatisticsDiv.style.gridRow = buildType.order*2
     parentElement.appendChild(buildListDiv)
 
     let buildStepsText = document.createTextNode('🚧 Will fetch and display the (status of) individual build steps.')
@@ -193,7 +193,7 @@ async function renderBuildType(buildType) {
     buildSteps.appendChild(buildStepsText)
     buildSteps.classList.add('buildSteps')
     buildSteps.classList.add('hidden')
-    buildSteps.style.order = (buildType.order*3)+3
+    buildSteps.style.gridRow = (buildType.order*2)+1
     parentElement.appendChild(buildSteps)
 
     // Add statusChanged when the last build status is different.
