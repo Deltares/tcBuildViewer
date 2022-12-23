f491c81e12439c57c3cd401fb41bf2b194b58abd
Robin van Westrenen <Robin.vanWestrenen@deltares.nl>
2022-12-23 10:56:45 +0100

    t

--- js/render.js
+++ js/render.js
@@ -388,7 +388,7 @@ async function renderBuildDetails(buildId,messages,tests,changes) {
 
             if (message.containsMessages && message.id != 0) {
                 let moreMessages = get_more_messages(buildId,message.id)
-                //addMessagesToElement(await moreMessages, messageP)
+                addMessagesToElement(await moreMessages, messageP)
             }
     
         })
