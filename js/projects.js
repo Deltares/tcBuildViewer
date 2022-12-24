9cc7f0f7025c2f5be253285c4a0356982c41eb07
robin-deltares <118373690+robin-deltares@users.noreply.github.com>
2022-12-24 03:19:26 +0100

    Update projects.js

--- js/projects.js
+++ js/projects.js
@@ -281,8 +281,6 @@ async function get_more_messages(buildId,messageId) {
 
     let messages = messagesJSON.messages.filter((message) => {return message.parentId == messageId})
 
-    console.log(messages)
-
     return messages
 
 }
