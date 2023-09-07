761e99b27a8c3e5a8c25920cd8815feda4760dde
Jeroen <jeroen.noort@deltares.nl>
2023-09-07 14:21:35 +0200

    add gettime to date

--- js/projects.js
+++ js/projects.js
@@ -163,7 +163,7 @@ async function add_builds_to_buildtype(buildType, parentProjectStats, parentProj
                     build[i].unixTime = tcTimeToUnix(build[i].finishEstimate)
                 }
                 else if (build[i].progress-info) {
-                    build[i].unixTime = (Date.now() + build[i].progress-info.leftSeconds * 1000)
+                    build[i].unixTime = (Date.now() + build[i].progress-info.leftSeconds * 1000).getTime()
                 }
 
                 renderBuild(build[i])
